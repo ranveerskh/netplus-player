223585e83daeb4ad6d40d0332c11bb4d9bdd7d75
ranveerskh <ranveerskh@gmail.com>
2026-08-29 14:55:42 -0400

    Delete electron directory

--- electron/preload.cjs
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("stbPlay", {
-  platform: process.platform,
-  installUpdate: (url) => ipcRenderer.invoke("download-and-install-update", String(url || "")),
-});
